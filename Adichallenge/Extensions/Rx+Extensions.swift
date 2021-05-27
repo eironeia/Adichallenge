b362140a411f28f12be873f24887436f67fa2059
Eironeia <acuellor.6@gmail.com>
2021-05-27 15:58:12 +0200

    First ViewController base implementation completed

---
+++ Adichallenge/Extensions/Rx+Extensions.swift
@@ -0,0 +1,8 @@
+//
+//  Rx+Extensions.swift
+//  Adichallenge
+//
+//  Created by Alex Cuello on 27/05/2021.
+//
+
+import Foundation
---
+++ Adichallenge/Extensions/UITableView+Extension.swift
@@ -0,0 +1,8 @@
+//
+//  UITableView+Extension.swift
+//  Adichallenge
+//
+//  Created by Alex Cuello on 27/05/2021.
+//
+
+import Foundation
